d57cdd09b2beaad462b8ea273006c5b2854d7766
lhy <hlicn@connect.ust.hk>
2023-02-17 09:55:17 +0800

    update predict_det.py

--- tools/infer/text/args.py
+++ tools/infer/text/args.py
@@ -3,7 +3,12 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("")
 
+
+    # mxocr
+    parser.add_argument("--input_images_path", type=str, required=True)
+    parser.add_argument("--config_path", type=str, required=True)
+    parser.add_argument("--parallel_num", type=str, required=False, default=1)
+    parser.add_argument("--infer_res_save_path", type=str, required=False, default=None)
 
     return parser.parse_args()
--- tools/infer/text/predict_det.py
+++ tools/infer/text/predict_det.py
@@ -1,32 +1,61 @@
+"""
+Prediction pipeline for detection tasks.
+"""
+from mindspore import Tensor
 from args import get_args
 from mindocr.data.det_dataset import transforms_dbnet_icdar15
-from mindocr.data.transforms.transform_factory import create_transforms
+from mindocr.data.transforms.transform_factory import create_transforms, run_transforms
+
 from mindocr.utils.visualize import draw_bboxes
+# TODO: change mxocr dir
+from mindocr.mxocr.src.demo.python.main import build_pipeline, image_sender
 
 
 class Detector(object):
     def __init__(self, args) -> None:
         self.det_algorithm = args.det_algorithm
+        self.config_path = args.config_path
+        self.parallel_num = args.parallel_num
+        self.input_queue = args.input_queue
+        self.infer_res_save_path = args.infer_res_save_path
+
         self.transform_pipeline = transforms_dbnet_icdar15(is_train="infer")
         self.transforms = create_transforms(self.transform_pipeline)
+        self.post_process = create_postprocess()
 
-    def infer_mslite(self, img):
-
-
-    def infer_mxocr(self, img):
+    def infer_mxocr(self, img: Tensor):
+        """
+        infer image using mxocr
+        :param img:
+        :return:
+        """
 
         return
 
+    # def infer_mslite(self, img):
+    #     pass
 
-    def post_processing(self, infer_res):
+    def post_processing(self, infer_res: Tensor):
 
 
     def __call__(self, img):
+        img = run_transforms(img, self.transforms)
+        infer_res = self.infer_mxocr(img)
+        res = self.post_process(infer_res)
 
-        self.infer_mslite(img)
-        self.infer_mxocr(img)
-        self.post_processing()
-
+        return res
 
 if __name__ == '__main__':
+    # parse args
+    args = get_args()
+    detector = Detector(args)
 
+    # load data
+
+
+    # infer
+
+    # visualize
+    draw_bboxes(image=, bboxes=, color=(255, 0, 0), thickness=2)
+
+    # logging
