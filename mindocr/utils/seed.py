41cf63b48edbf19a331979d4c0b60117c482ba1e
samithuang <285365963@qq.com>
2023-03-07 12:21:54 +0000

    fix random seed bug

--- mindocr/utils/seed.py
+++ mindocr/utils/seed.py
@@ -3,13 +3,14 @@ import random
 import numpy as np
 import mindspore as ms
 
-def set_seed(seed=42, rank=0):
+def set_seed(seed=42):
     '''
     seed: seed int
-    rank: rank id
+
+    Note: to ensure model init stability, rank_id is removed for seed.
     '''
-    if rank is None:
-        rank = 0
-    random.seed(seed+rank)
-    ms.set_seed(seed+rank)
-    np.random.seed(seed+rank)
+    #if rank is None:
+    #    rank = 0
+    random.seed(seed)
+    ms.set_seed(seed)
+    np.random.seed(seed)
