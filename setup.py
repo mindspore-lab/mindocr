21d269e16d4adcce1804750604847aa814abfddc
liangxhao <liangxhao@gmail.com>
2023-03-13 19:55:31 +0800

    adapt to lower version setuptools for setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 __version__ = '0.0.1'
 
@@ -13,21 +13,24 @@ setup(
         "Sources": "https://github.com/mindspore-lab/mindocr",
         "Issue Tracker": "https://github.com/mindspore-lab/mindocr/issues",
     },
-    description="A toolbox of vision models and algorithms based on MindSpore.",
+    description="A toolbox of OCR models and algorithms based on MindSpore.",
     license="Apache Software License 2.0",
     include_package_data=True,
-    package_dir={'mindocr.mindocr': 'mindocr', 'mindocr.tools': 'tools', 'mindocr.deploy': 'deploy'},
+    packages=['mindocr'],
+    package_dir={'mindocr': ""},
     entry_points={"console_scripts": ["mindocr=mindocr.deploy.mx_infer.infer_pipeline:main"]},
     install_requires=[
         "numpy >= 1.17.0",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
     test_suite="tests",
