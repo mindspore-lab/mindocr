57e82a62b1184c86168dcfd0aba3f7e6243e11ab
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2023-03-22 10:12:01 +0800

    refactor data augmentation pipeline

--- mindocr/data/transforms/iaa_augment.py
+++ mindocr/data/transforms/iaa_augment.py
@@ -1,83 +1,25 @@
-"""
-iaa transform
-"""
 import numpy as np
 import imgaug
 import imgaug.augmenters as iaa
 
 __all__ = ['IaaAugment']
 
-def compose_iaa_transforms(args, root=True):
-    if (args is None) or (len(args) == 0):
-        return None
-    elif isinstance(args, list):
-        if root:
-            sequence = [compose_iaa_transforms(value, root=False) for value in args]
-            return iaa.Sequential(sequence)
-        else:
-            return getattr(iaa, args[0])( *[_list_to_tuple(a) for a in args[1:]])
-    elif isinstance(args, dict):
-        cls = getattr(iaa, args['type'])
-        return cls(**{
-            k: _list_to_tuple(v)
-            for k, v in args['args'].items()
-        })
-    else:
-        raise ValueError('Unknown augmenter arg: ' + str(args))
 
-
-def _list_to_tuple(obj):
-    if isinstance(obj, list):
-        return tuple(obj)
-    return obj
-
-
-class IaaAugment():
-    def __init__(self, augmenter_args=None, **kwargs):
-        if augmenter_args is None:
-            augmenter_args = [{
-                'type': 'Fliplr',
-                'args': {
-                    'p': 0.5
-                }
-            }, {
-                'type': 'Affine',
-                'args': {
-                    'rotate': [-10, 10]
-                }
-            }, {
-                'type': 'Resize',
-                'args': {
-                    'size': [0.5, 3]
-                }
-            }]
-        self.augmenter = compose_iaa_transforms(augmenter_args)
+class IaaAugment:
+    def __init__(self, **augments):
+        self._augmenter = iaa.Sequential([getattr(iaa, aug)(**args) for aug, args in augments.items()])
 
     def __call__(self, data):
-        image = data['image']
-        shape = image.shape
+        aug = self._augmenter.to_deterministic()    # to augment an image and its keypoints identically
+        data['image'] = aug.augment_image(data['image'])
 
-        if self.augmenter:
-            aug = self.augmenter.to_deterministic()
-            data['image'] = aug.augment_image(image)
-            data = self.may_augment_annotation(aug, data, shape)
+        if 'polys' in data:
+            new_polys = []
+            for poly in data['polys']:
+                kps = imgaug.KeypointsOnImage([imgaug.Keypoint(p[0], p[1]) for p in poly], shape=data['image'].shape)
+                kps = aug.augment_keypoints(kps)
+                new_polys.append(np.array([[kp.x, kp.y] for kp in kps.keypoints]))
+
+            data['polys'] = np.array(new_polys) if isinstance(data['polys'], np.ndarray) else new_polys
+
         return data
-
-    def may_augment_annotation(self, aug, data, shape):
-        if aug is None:
-            return data
-
-        line_polys = []
-        for poly in data['polys']:
-            new_poly = self.may_augment_poly(aug, shape, poly)
-            line_polys.append(new_poly)
-        data['polys'] = np.array(line_polys)
-        return data
-
-    def may_augment_poly(self, aug, img_shape, poly):
-        keypoints = [imgaug.Keypoint(p[0], p[1]) for p in poly]
-        keypoints = aug.augment_keypoints(
-            [imgaug.KeypointsOnImage(
-                keypoints, shape=img_shape)])[0].keypoints
-        poly = [(p.x, p.y) for p in keypoints]
-        return poly
--- mindocr/data/transforms/transforms_factory.py
+++ mindocr/data/transforms/transforms_factory.py
@@ -1,17 +1,16 @@
-'''
+"""
 Create and run transformations from a config or predefined transformation pipeline
-'''
+"""
 from typing import List
 import numpy as np
 
 from .general_transforms import *
 from .det_transforms import *
 from .rec_transforms import *
-# TODO: merge transforms in modelzoo to det_transforms if verified to be correct
-from .modelzoo_transforms import *
 from .iaa_augment import *
 
 __all__ = ['create_transforms', 'run_transforms', 'transforms_dbnet_icdar15']
+
 
 # TODO: use class with __call__, to perform transformation
 def create_transforms(transform_pipeline, global_config=None):
@@ -26,7 +25,8 @@ def create_transforms(transform_pipeline, global_config=None):
     Returns:
         list of data transformation functions
     """
-    assert isinstance(transform_pipeline, list), (f'transform_pipeline config should be a list, but {type(transform_pipeline)} detected')
+    assert isinstance(transform_pipeline, list), \
+        f'transform_pipeline config should be a list, but {type(transform_pipeline)} detected'
 
     transforms = []
     for transform_config in transform_pipeline:
@@ -39,14 +39,14 @@ def create_transforms(transform_pipeline, global_config=None):
                 param.update(global_config)
             # TODO: assert undefined transform class
 
-            #print(trans_name, param)
+            # print(trans_name, param)
             transform = eval(trans_name)(**param)
             transforms.append(transform)
         elif callable(transform_config):
             transforms.append(transform_config)
         else:
             raise TypeError('transform_config must be a dict or a callable instance')
-        #print(global_config)
+        # print(global_config)
     return transforms
 
 
@@ -65,86 +65,72 @@ def run_transforms(data, transforms=None, verbose=False):
             return None
     return data
 
+
 # ---------------------- Predefined transform pipeline ------------------------------------
 def transforms_dbnet_icdar15(phase='train'):
-    '''
+    """
     Get pre-defined transform config for dbnet on icdar15 dataset.
     Args:
         phase: train, eval, infer
     Returns:
         list of dict for data transformation pipeline, which can be convert to functions by 'create_transforms'
-    '''
+    """
     if phase == 'train':
         pipeline = [
-                    {'DecodeImage': {
-                        'img_mode': 'BGR',
-                        'to_float32': False}},
-                    {'DetLabelEncode': None},
-                    {'MZResizeByGrid': {'divisor': 32, 'transform_polys': True}}, # prev in modelzoo, it doesn't transform polys
-                    {'MZRandomScaleByShortSide': {'short_side': 736}},
-                    {'IaaAugment': {'augmenter_args':
-                        [{'type': 'Affine',
-                            'args': {'rotate': [-10, 10]}
-                        },
-                        {'type': 'Fliplr',
-                            'args': {
-                                'p': 0.5}
-                        },
-                        ]}
-                    },
-                    {'MZRandomCropData':
-                            {'max_tries':100,
-                            'min_crop_side_ratio': 0.1,
-                            'crop_size': (640, 640)}},
-                    {'MZResizeByGrid': {'divisor': 32, 'transform_polys': True}},
-                    #{'MakeShrinkMap':
-                    {'MZMakeSegDetectionData':
-                        {'min_text_size': 8, 'shrink_ratio': 0.4}},
-                    #{'MakeBorderMap':
-                    {'MZMakeBorderMap':
-                        {'shrink_ratio': 0.4, 'thresh_min': 0.3, 'thresh_max': 0.7,
-                    }},
-                    {'MZRandomColorAdjust': {'brightness': 32.0 / 255, 'saturation':0.5, 'to_numpy':True}},
-                    #{'MZIrregularNormToCHW': None},
-                    {'NormalizeImage': {
-                        'bgr_to_rgb': True,
-                        'is_hwc': True,
-                        'mean' : [123.675, 116.28, 103.53],
-                        'std' : [58.395, 57.12, 57.375],
-                        }
-                    },
-                    {'ToCHWImage': None}
-                    ]
+            {'DecodeImage': {
+                'img_mode': 'RGB',
+                'to_float32': False}},
+            {'DetLabelEncode': None},
+            {'RandomScale': {'scale_range': [1.022, 3.0]}},
+            {'IaaAugment':
+                 {'Affine': {'rotate': [-10, 10]},
+                  'Fliplr': {'p': 0.5}}},
+            {'RandomCropWithBBox':
+                 {'max_tries': 100,
+                  'min_crop_ratio': 0.1,
+                  'crop_size': (640, 640)}},
+            {'ShrinkBinaryMap':
+                 {'min_text_size': 8, 'shrink_ratio': 0.4}},
+            {'BorderMap':
+                 {'shrink_ratio': 0.4, 'thresh_min': 0.3, 'thresh_max': 0.7,}},
+            {'RandomColorAdjust': {'brightness': 32.0 / 255, 'saturation': 0.5}},
+            {'NormalizeImage': {
+                'bgr_to_rgb': False,
+                'is_hwc': True,
+                'mean': [123.675, 116.28, 103.53],
+                'std': [58.395, 57.12, 57.375],
+            }
+            },
+            {'ToCHWImage': None}
+        ]
 
-    elif phase=='eval':
+    elif phase == 'eval':
         pipeline = [
-                    {'DecodeImage': {'img_mode': 'BGR', 'to_float32': False}},
-                    {'DetLabelEncode': None},
-                    {'MZResizeByGrid': {'divisor': 32, 'transform_polys': True}}, # prev in modelzoo, it doesn't transform polys
-
-                    {'MZScalePad': {'eval_size': [736, 1280]}},
-                    {'NormalizeImage': {
-                        'bgr_to_rgb': True,
-                        'is_hwc': True,
-                        'mean' : [123.675, 116.28, 103.53],
-                        'std' : [58.395, 57.12, 57.375],
-                        }
-                    },
-                    {'ToCHWImage': None}
-
-                    ]
+            {'DecodeImage': {'img_mode': 'RGB', 'to_float32': False}},
+            {'DetLabelEncode': None},
+            {'GridResize': {'factor': 32}},
+            {'ScalePadImage': {'target_size': [736, 1280]}},
+            {'NormalizeImage': {
+                'bgr_to_rgb': False,
+                'is_hwc': True,
+                'mean': [123.675, 116.28, 103.53],
+                'std': [58.395, 57.12, 57.375],
+            }
+            },
+            {'ToCHWImage': None}
+        ]
     else:
         pipeline = [
-                    {'DecodeImage': {'img_mode': 'BGR', 'to_float32': False}},
-                    {'MZResizeByGrid': {'divisor': 32, 'transform_polys': True}},
-                    {'MZScalePad': {'eval_size': [736, 1280]}},
-                    {'NormalizeImage': {
-                        'bgr_to_rgb': True,
-                        'is_hwc': True,
-                        'mean' : [123.675, 116.28, 103.53],
-                        'std' : [58.395, 57.12, 57.375],
-                        }
-                    },
-                    {'ToCHWImage': None}
-                    ]
+            {'DecodeImage': {'img_mode': 'RGB', 'to_float32': False}},
+            {'GridResize': {'factor': 32}},
+            {'ScalePadImage': {'target_size': [736, 1280]}},
+            {'NormalizeImage': {
+                'bgr_to_rgb': False,
+                'is_hwc': True,
+                'mean': [123.675, 116.28, 103.53],
+                'std': [58.395, 57.12, 57.375],
+            }
+            },
+            {'ToCHWImage': None}
+        ]
     return pipeline
