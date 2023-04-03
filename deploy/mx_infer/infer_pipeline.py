820135875c741a211fdd68acb74fac1df958ce31
liangxhao <liangxhao@gmail.com>
2023-04-03 21:48:48 +0800

    fix setup

--- deploy/mx_infer/infer_pipeline.py
+++ deploy/mx_infer/infer_pipeline.py
@@ -3,6 +3,7 @@ import sys
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.abspath(os.path.join(__dir__, '../../')))
+sys.path.insert(0, os.path.abspath(os.path.join(__dir__, '../../mindocr')))
 
 from deploy.mx_infer.args import get_args
 import deploy.mx_infer.pipeline as pipeline
