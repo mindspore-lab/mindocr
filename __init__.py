b9a7195d3f78932f7a2a459d3374902f6b657a99
Samit <285365963@qq.com>
2023-04-03 20:53:00 +0800

    fix setup import

--- __init__.py
+++ __init__.py
@@ -5,3 +5,4 @@ from .mindocr import data, losses, metrics, models, postprocess, utils
 
 __all__ = []
 __all__.extend(mindocr.__all__)
+__all__.extend(['data', 'losses', 'metrics', 'models', 'postprocess', 'utils'])
