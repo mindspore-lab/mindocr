9472b36f3ac774a155a2f51be0f26e1bdcbb2f2c
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2023-04-18 09:56:30 +0800

    modify MLT2017_Converter to support official dataset labels

--- tools/dataset_converters/mlt2017.py
+++ tools/dataset_converters/mlt2017.py
@@ -1,83 +1,49 @@
-import os
 import json
-import glob
+from pathlib import Path
 import tqdm
-class MLT2017_Converter(object):
-    '''
-    Format annotation to standard form for MLT2017 dataset. The original annotation file is a COCO-format JSON annotation file. 
-    When loaded with json library, it is a dictionary data with following keys:
-    dict_keys(['licenses', 'info', 'images', 'annotations', 'categories'])
-    An example of data['images'] (a list of dictionaries):
-    {'width': 400, 'date_captured': '', 'license': 0, 'flickr_url': '', 'file_name': '0060001.jpg', 'id': 60001, 'coco_url': '', 'height': 600}
-    An example of data['annotations'] (a list of dictionaries):
-    {'image_id': 60001, 'bbox': [218.0, 406.0, 138.0, 47.0], 'area': 6486.0, 'rec': [95, ..., 96], 'category_id': 1, 'iscrowd': 0, 'id': 1, 
-    'bezier_pts': [219.0, ..., 218.0, 452.0]}
-    'bbox' is defined by [x_min, y_min, width, height] in coco format.
 
-    self._format_det_label transforms the annotations into a single det label file with a format like:
-    0060001.jpg	[{"transcription": "the", "points":[[153, 347], ..., [177, 357]], 'beizer':[123,...,567]}]
-    '''
+
+class MLT2017_Converter(object):
+    """
+    Format annotation to standard form for MLT2017 dataset.
+    The ground truth is provided in terms of word bounding boxes. Bounding boxes are specified by the coordinates of
+    their four corners in a clock-wise manner. For each image a corresponding UTF-8 encoded text file is provided,
+    following the naming convention:
+        `gt_[image name].txt`
+    The text files are comma separated files, where each line corresponds to one text block in the image and gives
+    its bounding box coordinates (four corners, clockwise), its script and its transcription in the format:
+        `x1,y1,x2,y2,x3,y3,x4,y4,script,transcription`
+    Valid scripts are: "Arabic", "Latin", "Chinese", "Japanese", "Korean", "Bangla", "Symbols", "Mixed", "None"
+    Note that the transcription is anything that follows the 9th comma until the end of line. No escape characters are to be used.
+    If the transcription is provided as "###", then text block (word) is considered as "don't care".
+    """
     def __init__(self, path_mode='relative'):
-        self.path_mode = path_mode
-        self.CTLABELS =  [' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~']
-        self.vocabulary_size = len(self.CTLABELS)+1
+        self._relative = path_mode == 'relative'
 
     def convert(self, task='det', image_dir=None, label_path=None, output_path=None):
-        self.label_path = label_path
-        assert os.path.exists(label_path), f'{label_path} no exist!'
-        
+        label_path = Path(label_path)
+        assert label_path.exists(), f'{label_path} does not exist!'
+
         if task == 'det':
-            self._format_det_label(image_dir, self.label_path, output_path)
+            self._format_det_label(Path(image_dir), label_path, output_path)
         if task == 'rec':
             raise ValueError("MLT 2017 dataset has no cropped word images and recognition labels.")
 
-    def _decode_rec_ids_to_string(self, rec):
-        transcription = ""
-        for index in rec:
-            if index==self.vocabulary_size-1:
-                transcription+=u'口'
-            elif index<self.vocabulary_size-1:
-                transcription+=self.CTLABELS[index]
-        return transcription
+    def _format_det_label(self, image_dir: Path, label_path: Path, output_path: str):
+        with open(output_path, 'w', encoding='utf-8') as out_file:
+            images = sorted(image_dir.iterdir(), key=lambda path: int(path.stem.split('_')[-1]))    # sort by image id
+            for img_path in tqdm.tqdm(images, total=len(images)):
+                label = []
+                with open(label_path / ('gt_' + img_path.stem + '.txt'), 'r', encoding='utf-8') as f:
+                    for line in f.read().splitlines():
+                        line = line.split(',', 9)   # split the line by first 9 commas: 8 points + language
 
+                        points = [[int(line[i]), int(line[i + 1])] for i in range(0, 8, 2)]    # reshape points (4, 2)
+                        label.append({
+                            'language': line[8],
+                            'transcription': line[9],
+                            'points': points
+                        })
 
-    def _format_det_label(self, image_dir, label_path, output_path):
-
-        with open(output_path, 'w') as out_file:
-            coco_json_data = json.load(open(label_path, 'r'))
-            annotations = coco_json_data['annotations']
-            images_labels = {}
-            for annot in tqdm.tqdm(annotations, total=len(annotations)):
-                image_id = annot['image_id']
-                img_path = os.path.join(image_dir, '{:07d}'.format(image_id)+ ".jpg")
-                assert os.path.exists(img_path), f'{img_path} not exist! Please check the input image_dir {image_dir}'
-                if self.path_mode == 'relative':
-                    img_path = os.path.basename(img_path)
-                if img_path not in images_labels:
-                    images_labels[img_path] = []
-                
-                bbox = annot['bbox'] # [x_min, y_min, width, height]
-                bbox = [[bbox[0], bbox[1]], [bbox[0]+bbox[2], bbox[1]], [bbox[0]+bbox[2], bbox[1]+bbox[3]], [bbox[0], bbox[1]+bbox[3]]]
-                bbox = [[int(x[0]), int(x[1])] for x in bbox]
-                bezier = annot['bezier_pts']
-                transcription = self._decode_rec_ids_to_string(annot['rec']) # needs to translate from character ids to characters.
-                images_labels[img_path].append(
-                    {"transcription":transcription, "points": bbox, "bezier":bezier}
-                )
-            for img_path in images_labels: 
-                annotations = []
-                for annot_instance in images_labels[img_path]:
-                    annotations.append(annot_instance)
-                out_file.write(img_path + '\t' + json.dumps(
-                    annotations, ensure_ascii=False) + '\n')
-
-
-    # def _format_rec_label(self, label_path, output_path):
-    #     with open(output_path, 'w') as outf:
-    #         with open(label_path, 'r') as f:
-    #             for line in f:
-    #                 # , may occur in text
-    #                 sep_index = line.find(',')
-    #                 img_path = line[:sep_index].strip().replace('\ufeff', '')
-    #                 label = line[sep_index+1:].strip().replace("\"", "")
-    #                 outf.write(img_path + '\t' + label + '\n') 
+                img_path = img_path.name if self._relative else str(img_path)
+                out_file.write(img_path + '\t' + json.dumps(label, ensure_ascii=False) + '\n')
