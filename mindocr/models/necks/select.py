3f0a26853b08d9010dae67bbd8eb9336992d0b0b
HaoyangLI <417493727@qq.com>
2023-05-23 16:55:40 +0800

    fix neck `Select` bug in graph mode

--- mindocr/models/necks/select.py
+++ mindocr/models/necks/select.py
@@ -1,14 +1,18 @@
+from mindspore import nn
+
 __all__ = ['Select']
 
-class Select(object):
+
+class Select(nn.Cell):
     '''
     select feature from the backbone output.
     '''
     def __init__(self, in_channels, index=-1):
+        super().__init__()
         self.index = index
         self.out_channels = in_channels[index]
 
-    def __call__(self, x):
+    def construct(self, x):
         if isinstance(x, list) or isinstance(x, tuple):
             return x[self.index]
         else:
