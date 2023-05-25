2cd050de04cd6b6d648f71e72e014decd53270f9
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2023-05-25 15:04:40 +0800

    create DetBasePostprocess and move scaling there

--- mindocr/postprocess/det_postprocess.py
+++ mindocr/postprocess/det_postprocess.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Union
+from typing import Tuple, Union, List
 import cv2
 import numpy as np
 from mindspore import Tensor
@@ -9,7 +9,36 @@ from ..data.transforms.det_transforms import expand_poly
 __all__ = ['DBPostprocess']
 
 
-class DBPostprocess:
+class DetBasePostprocess:
+    """
+    Base class for all text detection postprocessings.
+
+    Args:
+        rescale_fields: name of fields to scale back to the shape of the original image.
+    """
+    def __init__(self, rescale_fields: List[str] = None):
+        self._scale_fields = rescale_fields
+
+    @staticmethod
+    def _scale_sample(sample: Union[List[np.ndarray], np.ndarray], shape: np.ndarray):
+        # shape: [h, w, scale_h, scale_w]
+        if isinstance(sample, np.ndarray):
+            result = np.round(sample * shape[:1:-1])
+        else:
+            result = [np.round(s * shape[:1:-1]) for s in sample]
+
+        return result
+
+    def scale(self, processed: dict, shapes: Union[Tensor, np.ndarray]) -> dict:
+        shapes = shapes.asnumpy() if isinstance(shapes, Tensor) else shapes
+        shapes[:, 2:] = 1 / shapes[:, 2:]   # reverse scale values from the dataloader
+        for field in self._scale_fields:
+            processed[field] = [self._scale_sample(sample, shape) for sample, shape in zip(processed[field], shapes)]
+
+        return processed
+
+
+class DBPostprocess(DetBasePostprocess):
     """
     DBNet & DBNet++ postprocessing pipeline: extracts polygons / rectangles from a binary map (heatmap) and returns
         their coordinates.
@@ -21,29 +50,29 @@ class DBPostprocess:
             (DBNet predicts shrunken text masks).
         output_polygon: output polygons or rectangles as the network's predictions.
         pred_name: heatmap's name used for polygons extraction.
-        scale_polygons: scale polygons to match the groundtruth (use it only in case when input shape to
-            the network != output shape, e.g. when the input shape is not multiple of 32).
+        rescale_fields: name of fields to scale back to the shape of the original image.
     """
     def __init__(self, binary_thresh: float = 0.3, box_thresh: float = 0.7, max_candidates: int = 1000,
-                 expand_ratio: float = 1.5, output_polygon=False, pred_name: str = 'binary', scale_polygons=False):
+                 expand_ratio: float = 1.5, output_polygon=False, pred_name: str = 'binary', rescale_fields: List[str] = None):
+        super().__init__(rescale_fields)
+
         self._min_size = 3
         self._binary_thresh = binary_thresh
         self._box_thresh = box_thresh
         self._max_candidates = max_candidates
         self._expand_ratio = expand_ratio
         self._out_poly = output_polygon
-        self._scale = scale_polygons
         self._name = pred_name
         self._names = {'binary': 0, 'thresh': 1, 'thresh_binary': 2}
 
-    def __call__(self, pred: Union[Tensor, Tuple[Tensor], np.ndarray], shape: Union[Tensor, np.ndarray] = None,
+    def __call__(self, pred: Union[Tensor, Tuple[Tensor], np.ndarray], shapes: Union[Tensor, np.ndarray] = None,
                  **kwargs) -> dict:
         """
         pred (Union[Tensor, Tuple[Tensor], np.ndarray]):
             binary: text region segmentation map, with shape (N, 1, H, W)
             thresh: [if exists] threshold prediction with shape (N, 1, H, W) (optional)
             thresh_binary: [if exists] binarized with threshold, (N, 1, H, W) (optional)
-        shape: network input image shapes, (N, 4). These 4 values represent input image [h, w, scale_h, scale_w]
+        shapes: network input image shapes, (N, 4). These 4 values represent input image [h, w, scale_h, scale_w]
         Returns:
             result (dict) with keys:
                 polys: np.ndarray of shape (N, K, 4, 2) for the polygons of objective regions if region_type is 'quad'
@@ -57,20 +86,25 @@ class DBPostprocess:
 
         segmentation = pred >= self._binary_thresh
 
-        # scale polygons in case when the prediction shape doesn't match the groundtruth shape
-        shapes = [None] * len(pred)
-        if self._scale:
-            shapes = shape.asnumpy() if isinstance(shape, Tensor) else shape
+        if shapes is not None:  # clip polygons within the visible region
+            shapes = shapes.asnumpy() if isinstance(shapes, Tensor) else shapes
+            dest_size = shapes[:1:-1] - 1
+        else:                   # clip polygons within the prediction shape otherwise
+            dest_size = np.array(pred.shape[:0:-1]) - 1
 
         polys, scores = [], []
-        for pr, segm, sh in zip(pred, segmentation, shapes):
-            sample_polys, sample_scores = self._extract_preds(pr, segm, sh)
+        for pr, segm, size in zip(pred, segmentation, dest_size):
+            sample_polys, sample_scores = self._extract_preds(pr, segm, size)
             polys.append(sample_polys)
             scores.append(sample_scores)
 
-        return {'polys': polys, 'scores': scores}
+        output = {'polys': polys, 'scores': scores}
+        if self._scale_fields:
+            output = self.scale(output, shapes)
 
-    def _extract_preds(self, pred: np.ndarray, bitmap: np.ndarray, shape: np.ndarray):
+        return output
+
+    def _extract_preds(self, pred: np.ndarray, bitmap: np.ndarray, dest_size: np.ndarray):
         outs = cv2.findContours(bitmap.astype(np.uint8), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         if len(outs) == 3:  # FIXME: update to OpenCV 4.x and delete this
             _, contours, _ = outs[0], outs[1], outs[2]
@@ -111,12 +145,7 @@ class DBPostprocess:
             # box = np.array(expand_poly(points, distance=box.area * self._expand_ratio / box.length, joint_type=pyclipper.JT_MITER))
             # assert box.shape[0] == 4, print(f'box shape is {box.shape}')
 
-            # predictions may not be the same size as the groundtruth => scale them
-            if self._scale:
-                # shape: [h, w, scale_h, scale_w]
-                poly = np.clip(np.round(poly * shape[:1:-1]), 0, shape[1::-1] - 1).astype(np.int16)
-
-            polys.append(poly)
+            polys.append(np.clip(np.round(poly), 0, dest_size).astype(np.int16))
             scores.append(score)
 
         if self._out_poly:
