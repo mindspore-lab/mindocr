473601e5363b7eb7377f92d3544e689e2b1945e6
heepengpeng <heepengpeng@foxmail.com>
2023-06-09 15:15:32 +0800

    [feat] add mmocr nrtr & satrn

--- deploy/py_infer/src/data_process/preprocess/transforms/rec_transforms.py
+++ deploy/py_infer/src/data_process/preprocess/transforms/rec_transforms.py
@@ -1,3 +1,4 @@
+import math
 import os
 import sys
 
@@ -10,7 +11,12 @@ sys.path.insert(0, mindocr_path)
 
 from mindocr.data.transforms import rec_transforms  # noqa
 
-__all__ = ["SVTRRecResizeImg", "RecResizeNormForInfer", "RecResizeNormForViTSTR"]
+__all__ = [
+    "SVTRRecResizeImg",
+    "RecResizeNormForInfer",
+    "RecResizeNormForViTSTR",
+    "RecResizeNormForMMOCR",
+]
 
 
 class SVTRRecResizeImg(rec_transforms.SVTRRecResizeImg):
@@ -68,4 +74,43 @@ class RecResizeNormForViTSTR(object):
         norm_img = np.expand_dims(img, -1)
         norm_img = norm_img.astype(np.float32) / 255.0
         data["image"] = norm_img
+        return data
+
+
+class RecResizeNormForMMOCR:
+    def __init__(
+        self,
+        height=32,
+        min_width=32,
+        max_width=160,
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        pad_width=160,
+    ):
+        self.height = height
+        self.min_width = min_width
+        self.max_width = max_width
+        self.mean = mean
+        self.std = std
+        self.pad_width = pad_width
+
+    def __call__(self, data):
+        img = data["image"]
+        ori_height, ori_width = img.shape[:2]
+        new_width = math.ceil(float(self.height) / ori_height * ori_width)
+        if "target_size" in data:
+            self.tar_h, self.tar_w = data["target_size"]
+            padded_img = np.zeros((self.tar_h, self.tar_w, 3), dtype=np.float32)
+        else:
+            padded_img = np.zeros((ori_height, self.pad_width, 3), dtype=np.float32)
+        if self.min_width is not None:
+            new_width = max(self.min_width, new_width)
+        if self.max_width is not None:
+            new_width = min(self.max_width, new_width)
+        if self.tar_w is not None:
+            new_width = min(self.tar_w, new_width)
+        resized_img = cv2.resize(img, (new_width, self.height), interpolation=cv2.INTER_LINEAR)
+        padded_img[:, :new_width, :] = resized_img
+        padded_img = (padded_img - self.mean) / self.std
+        data["image"] = padded_img.astype(np.float32)
         return data
