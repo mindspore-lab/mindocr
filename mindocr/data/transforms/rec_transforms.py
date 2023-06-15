424434d61ca7689161dab99ca9eddf64965ffac1
Samit <285365963@qq.com>
2023-06-15 21:07:41 +0800

    update format

--- mindocr/data/transforms/rec_transforms.py
+++ mindocr/data/transforms/rec_transforms.py
@@ -296,7 +296,7 @@ def resize_norm_img_chinese(img, image_shape):
     h, w = img.shape[0], img.shape[1]
     c = img.shape[2]
     ratio = w * 1.0 / h
-    #max_wh_ratio = min(max(max_wh_ratio, ratio), max_wh_ratio)
+    # max_wh_ratio = min(max(max_wh_ratio, ratio), max_wh_ratio)
     imgW = int(imgH * max_wh_ratio)
     if math.ceil(imgH * ratio) > imgW:
         resized_w = imgW
