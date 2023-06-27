9f35b2bfde708e1b8497f5f4714e1b3d6835498c
liangxhao <liangxhao@gmail.com>
2023-06-27 14:35:11 +0800

    optim some infer yaml, readme and parallel (#450)

--- deploy/py_infer/src/data_process/postprocess/postprocess_mapping.py
+++ deploy/py_infer/src/data_process/postprocess/postprocess_mapping.py
@@ -4,14 +4,12 @@ POSTPROCESS_MAPPING_OPS = {
     # det
     "DBPostprocess": det_postprocess.DBPostprocess,
     "EASTPostprocess": det_postprocess.EASTPostprocess,
-    "DistillationDBPostProcess": det_postprocess.DBPostprocess,
     "PSEPostprocess": det_postprocess.PSEPostprocess,
     "SASTPostprocess": det_postprocess.SASTPostprocess,
     "FCEPostprocess": det_postprocess.FCEPostprocess,
     # rec
     "RecCTCLabelDecode": rec_postprocess.RecCTCLabelDecode,
-    "CTCLabelDecode": rec_postprocess.RecCTCLabelDecode,
-    "DistillationCTCLabelDecode": rec_postprocess.RecCTCLabelDecode,
+    "RecAttnLabelDecode": rec_postprocess.RecAttnLabelDecode,
     "ViTSTRLabelDecode": rec_postprocess.ViTSTRLabelDecode,
     "AttentionLabelDecode": rec_postprocess.AttentionLabelDecode,
     # cls
