a341b0640cbda6516079b31d6000d999eb4192f4
VictorHe-1 <80800595+VictorHe-1@users.noreply.github.com>
2023-07-13 11:54:47 +0800

    PSENet ctw1500 inference support (#500)

--- deploy/py_infer/src/utils/adapted/mindocr_models.py
+++ deploy/py_infer/src/utils/adapted/mindocr_models.py
@@ -7,6 +7,7 @@ MINDOCR_MODELS = {
     "en_ms_det_dbnetpp_resnet50": "det/dbnet/db++_r50_icdar15.yaml",
     "en_ms_det_psenet_resnet152": "det/psenet/pse_r152_icdar15.yaml",
     "en_ms_det_psenet_resnet50": "det/psenet/pse_r50_icdar15.yaml",
+    "ch_ms_det_psenet_resnet152": "det/psenet/pse_r152_ctw1500.yaml",
     "en_ms_rec_crnn_resnet34": "rec/crnn/crnn_resnet34.yaml",
     "en_ms_det_east_resnet50": "det/east/east_r50_icdar15.yaml",
     "en_ms_det_east_mobilenetv3": "det/east/east_mobilenetv3_icdar15.yaml",
