e6782940cea3cf6de12f0c4fa447468263bf1e18
Mike Cheung <zhtmike@gmail.com>
2023-07-18 15:20:41 +0800

    add mode in test

--- tests/ut/test_models.py
+++ tests/ut/test_models.py
@@ -16,17 +16,18 @@ from mindocr.models import build_model
 all_model_names = mindocr.list_models()
 print("Registered models: ", all_model_names)
 
+# yaml path and mode (0: graph, 1: pynative)
 all_yamls = [
-    "configs/det/dbnet/db_r50_icdar15.yaml",
-    "configs/rec/crnn/crnn_resnet34.yaml",
-    "configs/rec/crnn/crnn_vgg7.yaml",
-    "configs/rec/master/master_resnet31.yaml",
-    "configs/cls/mobilenetv3/cls_mv3.yaml",
+    ("configs/det/dbnet/db_r50_icdar15.yaml", [0, 1]),
+    ("configs/rec/crnn/crnn_resnet34.yaml", [0, 1]),
+    ("configs/rec/crnn/crnn_vgg7.yaml", [0, 1]),
+    ("configs/rec/master/master_resnet31.yaml", [0]),
+    ("configs/cls/mobilenetv3/cls_mv3.yaml", [0, 1]),
 ]
 print("All config yamls: ", all_yamls)
 
 
-def _infer_dummy(model, task="det", verbose=True):
+def _infer_dummy(model, task="det", mode=0, verbose=True):
     print(task)
 
     bs = 8
@@ -38,7 +39,7 @@ def _infer_dummy(model, task="det", verbose=True):
         c, h, w = 3, 640, 640
     print("net input shape: ", bs, c, h, w)
     x = ms.Tensor(np.random.rand(bs, c, h, w), dtype=ms.float32)
-    ms.set_context(mode=ms.PYNATIVE_MODE)
+    ms.set_context(mode=mode)
 
     def predict(model, x):
         start = time.time()
@@ -79,27 +80,19 @@ def test_model_by_name(model_name, pretrained):
     print("model created")
 
 
-@pytest.mark.parametrize("yaml_fp", all_yamls)
-def test_model_by_yaml(yaml_fp):
-    print(yaml_fp)
-    with open(yaml_fp) as fp:
+@pytest.mark.parametrize("yaml_and_modes", all_yamls)
+def test_model_by_yaml(yaml_and_modes):
+    print(yaml_and_modes)
+    yaml_path, modes = yaml_and_modes
+    with open(yaml_path) as fp:
         config = yaml.safe_load(fp)
 
-    task = yaml_fp.split("/")[1]
-
-    if task == "rec":
-        from mindocr.postprocess.rec_postprocess import RecCTCLabelDecode
-
-        dict_path = config["common"]["character_dict_path"]
-        # read dict path and get class nums
-        rec_info = RecCTCLabelDecode(character_dict_path=dict_path)
-        num_classes = len(rec_info.character)
-        config["model"]["head"]["out_channels"] = num_classes
-        print("num characters: ", num_classes)
+    task = yaml_path.split("/")[1]
 
     model_config = config["model"]
     model = build_model(model_config)
-    _infer_dummy(model, task=task)
+    for mode in modes:
+        _infer_dummy(model, task=task, mode=mode)
 
 
 if __name__ == "__main__":
