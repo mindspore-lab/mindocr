86292c4691492f74cdcc6056d1d8e8bce29e652f
Songyuanwei <52945530+Songyuanwei@users.noreply.github.com>
2023-07-19 18:08:30 +0800

    add ut test for model train and infer (#512)
    Co-authored-by: songyuanwei <song.yuanwei@huawei.com>

--- tests/ut/test_models.py
+++ tests/ut/test_models.py
@@ -1,14 +1,11 @@
+import subprocess
 import sys
+
+from _common import gen_dummpy_data, update_config_for_CI
 
 sys.path.append(".")
 
-import time
-
-import numpy as np
 import pytest
-import yaml
-
-import mindspore as ms
 
 import mindocr
 from mindocr.models import build_model
@@ -18,55 +15,14 @@ print("Registered models: ", all_model_names)
 
 all_yamls = [
     "configs/det/dbnet/db_r50_icdar15.yaml",
+    "configs/det/dbnet/db++_r50_icdar15.yaml",
     "configs/rec/crnn/crnn_resnet34.yaml",
-    "configs/rec/crnn/crnn_vgg7.yaml",
+    "configs/rec/master/master_resnet31.yaml",
+    "configs/rec/rare/rare_resnet34.yaml",
+    "configs/rec/svtr/svtr_tiny.yaml",
     "configs/cls/mobilenetv3/cls_mv3.yaml",
 ]
 print("All config yamls: ", all_yamls)
-
-
-def _infer_dummy(model, task="det", verbose=True):
-    print(task)
-
-    bs = 8
-    if task == "rec":
-        c, h, w = 3, 32, 100
-    elif task == "cls":
-        c, h, w = 3, 48, 192
-    else:
-        c, h, w = 3, 640, 640
-    print("net input shape: ", bs, c, h, w)
-    x = ms.Tensor(np.random.rand(bs, c, h, w), dtype=ms.float32)
-    ms.set_context(mode=ms.PYNATIVE_MODE)
-
-    def predict(model, x):
-        start = time.time()
-        y = model(x)
-        print(time.time() - start)
-        return y
-
-    def predict_parts(model, x):
-        bout = model.backbone(x)
-        print("backbone output feature shapes: ")
-        for ftr in bout:
-            print("\t", ftr.shape)
-        nout = model.neck(bout)
-        print("neck output shape: ", nout.shape)
-        hout = model.head(nout)
-        print("head output shape: ")
-        if isinstance(hout, tuple):
-            for ho in hout:
-                print(ho.shape)
-        else:
-            print(hout.shape)
-
-        return hout
-
-    if verbose:
-        out = predict_parts(model, x)
-    else:
-        out = predict(model, x)
-    return out
 
 
 @pytest.mark.parametrize("model_name", all_model_names)
@@ -74,34 +30,27 @@ def _infer_dummy(model, task="det", verbose=True):
 def test_model_by_name(model_name, pretrained):
     print(model_name)
     build_model(model_name, pretrained=pretrained)
-    # _infer_dummy(model)
     print("model created")
+
+
+gen_dummpy_data(task="det")
+gen_dummpy_data(task="rec")
+gen_dummpy_data(task="cls")
 
 
 @pytest.mark.parametrize("yaml_fp", all_yamls)
 def test_model_by_yaml(yaml_fp):
-    print(yaml_fp)
-    with open(yaml_fp) as fp:
-        config = yaml.safe_load(fp)
-
     task = yaml_fp.split("/")[1]
+    dummpy_config_fp = update_config_for_CI(yaml_fp, task, val_while_train=True)
+    # ---------------- test running train.py using the toy data ---------
 
-    if task == "rec":
-        from mindocr.postprocess.rec_postprocess import RecCTCLabelDecode
+    cmd = f"python tools/train.py --config {dummpy_config_fp}"
 
-        dict_path = config["common"]["character_dict_path"]
-        # read dict path and get class nums
-        rec_info = RecCTCLabelDecode(character_dict_path=dict_path)
-        num_classes = len(rec_info.character)
-        config["model"]["head"]["out_channels"] = num_classes
-        print("num characters: ", num_classes)
-
-    model_config = config["model"]
-    model = build_model(model_config)
-    _infer_dummy(model, task=task)
+    print(f"Running command: \n{cmd}")
+    ret = subprocess.call(cmd.split(), stdout=sys.stdout, stderr=sys.stderr)
+    assert ret == 0, "Training fails"
 
 
 if __name__ == "__main__":
     print(all_model_names)
-    test_model_by_name(all_model_names[2], True)
-    # test_model_by_yaml(all_yamls[1])
+    test_model_by_yaml(all_yamls[0])
