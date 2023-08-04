9ee81576f99149affa7152716e54a1b118a8f459
liangxhao <liangxhao@gmail.com>
2023-08-04 11:20:32 +0800

    add dynamic shape support for infer (#545)

--- deploy/py_infer/src/infer/infer_det.py
+++ deploy/py_infer/src/infer/infer_det.py
@@ -12,7 +12,7 @@ class TextDetector(InferBase):
         super(TextDetector, self).__init__(args)
 
     def _init_preprocess(self):
-        self.preprocess_ops = build_preprocess(self.args.det_config_path)
+        self.preprocess_ops = build_preprocess(self.args.det_config_path, self.requires_gear_hw)
 
     def _init_model(self):
         self.model = Model(
@@ -24,24 +24,34 @@ class TextDetector(InferBase):
 
         shape_type, shape_value = self.model.get_shape_details()
 
-        if shape_type not in (ShapeType.DYNAMIC_IMAGESIZE, ShapeType.STATIC_SHAPE):
-            raise ValueError("Input shape must be static shape or dynamic image_size for detection model.")
-
-        # Only support NCHW format for net_inputs[0].
-        # If multi input, the target_size may be invalid.
+        # Only check inputs[0] currently
         shape_value = shape_value[0]
 
-        if shape_type == ShapeType.DYNAMIC_IMAGESIZE:
-            batchsize, _, hw_list = shape_value
-        else:
-            batchsize, _, h, w = shape_value
-            hw_list = [(h, w)]
+        # check batch_size
+        # assuming that the first dim is batch size, make sure that the batch_size must support 1
+        batch_size = shape_value[0]
+        if (batch_size not in [1, -1]) and (
+            isinstance(batch_size, (tuple, list)) and 1 not in batch_size  # dynamic batch size list: [1, ...]
+        ):
+            raise ValueError("Input batch size must support 1 for detection model.")
+        batch_size = [batch_size] if not isinstance(batch_size, (tuple, list)) else batch_size
 
-        if batchsize != 1:
-            raise ValueError("Input batch size must be 1 for detection model.")
+        # check h/w
+        if shape_type == ShapeType.DYNAMIC_SHAPE:
+            # without any checks, and assuming that h/w is dynamic
+            # if not, may throw exceptions in model_infer for un-matched h/w
+            hw_list = []
+        elif shape_type == ShapeType.DYNAMIC_IMAGESIZE:  # only support NCHW
+            *_, hw_list = shape_value
+        else:  # static shape or dynamic batch size
+            if len(shape_value) == 4:  # only support NCHW
+                *_, h, w = shape_value
+                hw_list = [(h, w)]
+            else:
+                hw_list = []  # without any checks
 
-        self._hw_list = hw_list
-        self._bs_list = [batchsize]
+        self._hw_list = tuple(hw_list)
+        self._bs_list = tuple(batch_size)
 
     def _init_postprocess(self):
         self.postprocess_ops = build_postprocess(self.args.det_config_path, rescale_fields=["polys"])
@@ -49,16 +59,24 @@ class TextDetector(InferBase):
     def get_params(self):
         return {"det_batch_num": self._bs_list}
 
-    def __call__(self, image: np.ndarray):
-        data = self.preprocess(image)
-        pred = self.model_infer(data)
-        polys = self.postprocess(pred, data["shape_list"])
+    def __call__(self, images: List[np.ndarray]) -> List:
+        outputs = []
+        for image in images:
+            data = self.preprocess(image)
+            pred = self.model_infer(data)
+            polys = self.postprocess(pred, data["shape_list"])
+            outputs.append(polys)
 
-        return polys
+        return outputs
 
     def preprocess(self, image: np.ndarray) -> Dict:
-        target_size = gear_utils.get_matched_gear_hw(cv_utils.get_hw_of_img(image), self._hw_list)
-        return self.preprocess_ops([image], target_size=target_size)
+        if self.requires_gear_hw:
+            target_size = gear_utils.get_matched_gear_hw(cv_utils.get_hw_of_img(image), self._hw_list)
+            data = self.preprocess_ops([image], target_size=target_size)
+        else:
+            data = self.preprocess_ops([image])
+
+        return data
 
     def model_infer(self, data: Dict) -> List[np.ndarray]:
         return self.model.infer(data["net_inputs"])
