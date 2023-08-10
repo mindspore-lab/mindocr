5b08394ff53c32e92d6299c29e6970c479cedc14
VictorHe-1 <80800595+VictorHe-1@users.noreply.github.com>
2023-08-10 10:35:04 +0800

    Add LayoutXLMForSer and LayoutXLMForRe backbone (#574)

--- mindocr/models/backbones/layout_xlm_ms/__init__.py
+++ mindocr/models/backbones/layout_xlm_ms/__init__.py
@@ -1,5 +1,6 @@
 from .configuration import LayoutXLMPretrainedConfig
-from .layoutxlm import LayoutXLMModel
+from .layoutxlm import LayoutXLMModel, layoutxlm_for_re, layoutxlm_for_ser
 from .tokenizer import LayoutXLMTokenizer
 
-__all__ = ["LayoutXLMModel", "LayoutXLMPretrainedConfig", "LayoutXLMTokenizer"]
+__all__ = ["LayoutXLMModel", "LayoutXLMPretrainedConfig",
+           "LayoutXLMTokenizer", "layoutxlm_for_ser", "layoutxlm_for_re"]
