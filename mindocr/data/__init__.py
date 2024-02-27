372675bc542dc356e7b0ab752d08137e91ec9c8d
Bourn3z <50265953+Bourn3z@users.noreply.github.com>
2024-02-27 22:38:11 +0800

    fix for overfit testcase (#674)

--- mindocr/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import builder, transforms
-from .builder import *
-from .transforms import *
-
-__all__ = []
-__all__.extend(builder.__all__)
-__all__.extend(transforms.__all__)
--- mindocr/models/heads/builder.py
+++ mindocr/models/heads/builder.py
@@ -25,9 +25,6 @@ from .det_db_head import DBHead, DBHeadEnhance
 from .det_east_head import EASTHead
 from .det_fce_head import FCEHead
 from .det_pse_head import PSEHead
-from .kie_relationextraction_head import RelationExtractionHead
-from .kie_tokenclassification_head import TokenClassificationHead
-from .rec_abinet_head import ABINetHead
 from .rec_attn_head import AttentionHead
 from .rec_ctc_head import CTCHead
 from .rec_master_decoder import MasterDecoder
