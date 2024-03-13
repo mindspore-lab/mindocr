ff86cac41d572ab535eac3d329e7490708b474bf
w00517672 <wutiancheng@huawei.com>
2024-03-13 16:19:22 +0800

    add generation

---
+++ mindocr/generation/__init__.py
@@ -0,0 +1,8 @@
+from .generation_config import *
+from .logits_process import *
+from .text_generator import *
+
+__all__ = []
+__all__.extend(generation_config.__all__)
+__all__.extend(logits_process.__all__)
+__all__.extend(text_generator.__all__)
