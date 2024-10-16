f177a2fad316d4a717e2875bcc1e6f22dcfa9f83
Xv-M-S <1205507925@qq.com>
2024-10-16 17:47:54 +0800

    根据CI测试报告修改

--- examples/license_plate_detection_and_recognition/split.py
+++ examples/license_plate_detection_and_recognition/split.py
@@ -12,14 +12,14 @@ def copy_files_to_directory(file_paths, target_dir):
 
 def read_train_files(train_txt_path):
     file_paths = []
-    with open(train_txt_path, 'r') as file:
+    with open(train_txt_path, "r") as file:
         for line in file:
             file_path = line.strip()
             file_paths.append(file_path)
     return file_paths
 
-txt_paths = ["./splits/train.txt","./splits/test.txt","./splits/val.txt"]
-target_dirs = ["ccpd_train","ccpd_test","ccpd_val"]
+txt_paths = ["./splits/train.txt", "./splits/test.txt", "./splits/val.txt"]
+target_dirs = ["ccpd_train", "ccpd_test", "ccpd_val"]
 
 for i in range(3):
     txt_path = txt_paths[i]
